bdde0e3ea0c39044b78a7b2f66dd9257b280a9d1
Matheus Queiroz <Kapazofarias@gmail.com>
2024-01-07 14:32:28 -0300

    Projeto-Teste-Unitario

--- TestesUnitarios.Desafio.Console/Program.cs
+++ TestesUnitarios.Desafio.Console/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//caso dê erro de repositorio use "cd" para direcionar para os repositorios: "Trilha-net-testes-unitarios-desafio, TestesUnitarios.Desafio.tests"
+
